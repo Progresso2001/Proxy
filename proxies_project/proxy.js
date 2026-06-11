d5c8c22b4665da74749773afa6a7169db18034f5
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-11 16:06:28 +0530

    usando o deleteProperty

--- proxies_project/proxy.js
+++ proxies_project/proxy.js
@@ -1,22 +1,43 @@
+// const pessoa = {
+//     nome:'DevJocas',
+//     idade:20
+// }
+// const handler = {
+//     get(objeto, propriedade){
+//         console.log(`Acessar o valor da propriedade: ${propriedade}`)
+//         return objeto[propriedade]
+//     },
+//     set(objeto, propriedade, valor){
+//         if(propriedade === 'idade' && valor < 0){
+//             throw new Error('Idade nao pode ser negativo')
+//         }
+//         objeto[propriedade] = valor
+//         return true
+//     }
+// }
+// const proxy = new Proxy(pessoa, handler)
+// // uso
+// console.log(proxy.nome)
+// proxy.idade =  30
+// console.log(proxy)
+
 const pessoa = {
-    nome:'DevJocas',
-    idade:20
+    nome:"Joaquim",
+    idade: 23,
+    senha:'abcd123@'
 }
 const handler = {
-    get(objeto, propriedade){
-        console.log(`Acessar o valor da propriedade: ${propriedade}`)
-        return objeto[propriedade]
-    },
-    set(objeto, propriedade, valor){
-        if(propriedade === 'idade' && valor < 0){
-            throw new Error('Idade nao pode ser negativo')
+    deleteProperty(obj, prop){
+        if(prop==='senha'){
+            console.error("Não pode alterar a senha")
+            return false
         }
-        objeto[propriedade] = valor
-        return true
+        console.log(`Deletando propriedade: ${prop}`)
+        return delete obj[prop]
     }
 }
-const proxy = new Proxy(pessoa, handler)
-// uso
-console.log(proxy.nome)
-proxy.idade =  30
+
+const proxy =  new Proxy(pessoa, handler)
+delete proxy.senha
+delete proxy.idade
 console.log(proxy)
